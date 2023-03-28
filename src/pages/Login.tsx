47f91364d25320ba72aa767ef7723b4e314585a6
yhhs12 <yhhs12@naver.com>
2023-03-28 20:28:00 +0900

    feat(react):commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,41 @@
+import MainButton from "@/styles/ui-components/styled-button";
+
 const Login = () => {
   return (
     <section className="w-full min-h-screen bg-red-50">
-      <h1>화면 연습용</h1>
-      <div className="w-full h-28 flex-col gap-2 bg-blue-100">
-        <form action="/login" method="post">
-          <input
-            className="border border-gray-400"
-            type="text"
-            name="username"
-          />
-          <input
-            className="border border-gray-400"
-            type="password"
-            name="password"
-          />
-        </form>
+      <div className="w-full flex flex-col gap-32 justify-center items-center">
+        <h1>화면 연습용</h1>
+        <div className="w-full h-28 flex flex-col items-center gap-2 bg-blue-100">
+          <form action="/login" method="post">
+            <input
+              className="border border-gray-400"
+              type="text"
+              name="username"
+            />
+            <input
+              className="border border-gray-400"
+              type="password"
+              name="password"
+            />
+          </form>
+          <MainButton className="w-[50%]">로그인</MainButton>
+        </div>
+        <div className="w-full h-28 flex-col gap-2 bg-green-200">
+          <form action="/login" method="post">
+            <input
+              className="border border-gray-400"
+              type="text"
+              name="username"
+            />
+            <input
+              className="border border-gray-400"
+              type="password"
+              name="password"
+            />
+          </form>
+        </div>
+        <h1></h1>
       </div>
-      <div className="w-full h-28 flex-col gap-2 bg-green-200">
-        <form action="/login" method="post">
-          <input
-            className="border border-gray-400"
-            type="text"
-            name="username"
-          />
-          <input
-            className="border border-gray-400"
-            type="password"
-            name="password"
-          />
-        </form>
-      </div>
-      <h1></h1>
     </section>
   );
 };
